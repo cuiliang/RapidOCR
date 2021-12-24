dc119b27f2d6bbb32c60bcf4bca5c73970b682b8
CuiLiang <cuiliang.bj@gmail.com>
2021-12-24 21:51:13 +0800

    增加多进程、多线程

--- ocrapi/main.py
+++ ocrapi/main.py
@@ -6,7 +6,16 @@ import base64
 
 import cv2
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
+from gevent import monkey
+from gevent.pywsgi import WSGIServer # pip install gevent
+monkey.patch_all()
+
+from multiprocessing import cpu_count, Process
+import datetime
+import os
+
 
 from task import detect_recognize
 
@@ -20,6 +29,54 @@ app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
 def index():
     return "Hello world."
 
+@app.route("/cppla", methods=['GET'])
+def function_benchmark():
+    return jsonify(
+        {
+            "status": "ok",
+            "time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+            "pid": os.getpid()
+        }
+    ), 200
+
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    if request.method == 'POST':
+        url_get = request.get_json()
+        url_get = str(url_get).split(',')[1]
+
+        image = base64.b64decode(url_get)
+        nparr = np.frombuffer(image, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        if image.ndim == 2:
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        return detect_recognize(image)
+
+
+def run(MULTI_PROCESS):
+    if MULTI_PROCESS == False:
+        WSGIServer(('0.0.0.0', 8080), app).serve_forever()
+    else:
+        mulserver = WSGIServer(('0.0.0.0', 8080), app)
+        mulserver.start()
+
+        def server_forever():
+            mulserver.start_accepting()
+            mulserver._stop_event.wait()
+
+        for i in range(cpu_count()):
+            p = Process(target=server_forever)
+            p.start()
+
+
+if __name__ == "__main__":
+    # 单进程 + 协程
+    # run(False)
+    # 多进程 + 协程
+    run(True)
+
+'''
 
 @app.route('/ocr', methods=['POST'])
 def ocr():
@@ -49,6 +106,7 @@ if __name__ == '__main__':
             f'launching gevent server... on {host}:{port}'
         )
         
-        from gevent.pywsgi import WSGIServer # pip install gevent
+        
         http_server = WSGIServer((host, port), app)
         http_server.serve_forever()
+'''
---
+++ ocrapi/main2.py
@@ -0,0 +1,72 @@
+# !/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @File: main.py
+# @Author: Max
+import base64
+
+import time
+import cv2
+import numpy as np
+from flask import Flask, render_template, request
+
+from task import detect_recognize
+
+app = Flask(__name__)
+
+# 设置上传文件大小
+app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
+
+
+@app.route('/')
+def index():
+    time.sleep(2)
+    print("index")
+    return "Hello world."
+
+def do_ocr(img_base64):
+    img_base64 = str(img_base64).split(',')[1]
+
+    image = base64.b64decode(img_base64)
+    nparr = np.frombuffer(image, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    if image.ndim == 2:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    return detect_recognize(image)
+
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    print("start ocr.")
+    if request.method == 'POST':
+        json_content = request.get_json()
+        file_base64 = json_content["file"]
+        result = do_ocr(file_base64)
+        return result
+
+
+
+if __name__ == '__main__':
+    debug = False
+    if debug:
+        app.run(
+            host='127.0.0.1',
+            port=8080,
+            debug=True,
+            threaded=True,
+            # processes=True,
+        )
+    else:
+        host = '0.0.0.0'
+        port = 8080
+        print(
+            f'launching server... on {host}:{port}'
+        )
+        
+
+        from waitress import serve # pip install waitress
+        serve(
+            app,
+            host=host,
+            port=port,
+            threads=20
+        )
