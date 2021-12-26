ffd9004ae709f98b2e39f2ac2d3c40f42ccaed2a
CuiLiang <cuiliang.bj@gmail.com>
2021-12-26 21:26:22 +0800

    返回合并好的行和区域列表

--- ocrapi/task.py
+++ ocrapi/task.py
@@ -25,6 +25,65 @@ text_sys = TextSystem(
     cls_model_path=cls_model_path
 )
 
+# 合并位置和结果列表
+def merge_list(dt_boxes, dt_data):
+    
+    # 获得某个块的Rect坐标，left,top,right,height. pos_list: [[x,y],[x,y],[x,y],[x,y]]
+    def get_rect(pos_list):
+        #print('pos_list',pos_list)
+
+        left = pos_list[0][0]
+        right =  pos_list[0][0]
+        top =  pos_list[0][1]
+        bottom =  pos_list[0][1]
+        for pos in pos_list:
+            if pos[0] < left:
+                left = pos[0]
+            if pos[0] > right:
+                right = pos[0] 
+            if pos[1] < top:
+                top = pos[1]
+            if pos[1] > bottom:
+                bottom = pos[1]
+        return dict(left = left, top = top, right = right ,bottom = bottom)
+
+    list = []
+    for i in range(len(dt_boxes)):
+        list.append(dict(rect = get_rect(dt_boxes[i]), text=dt_data[i][1]))
+    return list
+
+# 获取合并后的文本列表
+from io import StringIO
+
+def get_lines(region_list):
+    #print('region_list[0]',region_list[0])
+    lines = StringIO()
+    top = region_list[0]['rect']['top']
+    delta = 12  # 判断是新行的高度
+    start_new_line = True # 是否开始了一个新的行
+
+    for region in region_list:
+        item_top = region['rect']['top']
+        item_text = region['text']
+        if top > item_top - delta and top < item_top + delta: # 在同一行中
+            if start_new_line:
+                lines.write(item_text)
+                start_new_line = False
+            else:
+                lines.write('  ')
+                lines.write(item_text)
+        else: #开始了新的一行
+            lines.write('\n')            
+            if (item_top - top > delta*3): # 高度差别比较大，使用两个空行。
+                lines.write('\n') 
+            start_new_line = True
+            top = item_top
+            lines.write(item_text)           
+    
+    return lines.getvalue()
+
+
+
 
 def detect_recognize(image_path):
     if isinstance(image_path, str):
@@ -66,14 +125,28 @@ def detect_recognize(image_path):
 
         dt_boxes = [i.tolist() for i in dt_boxes]
 
-    return json.dumps({
+
+    start_time = time.perf_counter ()
+
+    regions = merge_list(dt_boxes, rec_res_data) # 合并好的区域列表。 [{rect:{left,top,right,bottom}, text:'text'}]
+    lines = get_lines(regions) # 合并好的文本
+   
+
+    dump = json.dumps({
         # 'image': img,
         'result': {
-            "dt_boxes": dt_boxes,
-            "rec_res_data": rec_res_data,
+            #"dt_boxes": dt_boxes,
+            #"rec_res_data": rec_res_data,
+            "regions": regions,
+            "lines": lines
         },
         'info': {
             'total_elapse': elapse,
             'elapse_part': elapse_part,
         },
     })
+    
+    end_time = time.perf_counter ()
+    print('=====耗时',(end_time - start_time)*1000)
+
+    return dump
