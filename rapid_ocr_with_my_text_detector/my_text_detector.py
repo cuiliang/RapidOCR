85aa7a04467f5a4935f82b5b8d463d932dfcc5ca
CuiLiang <cuiliang.bj@gmail.com>
2021-12-27 11:05:15 +0800

    来自地球的自定义检测算法。

---
+++ rapid_ocr_with_my_text_detector/my_text_detector.py
@@ -0,0 +1,122 @@
+import cv2
+import numpy as np
+import time
+
+
+def do_something(
+    my_image, 
+    dilation_kernel=(5, 5),
+    output_image=True,
+):
+    """
+    do dilation or find contour
+    """
+    image_h, image_w = my_image.shape
+
+    #dilation
+    kernel = np.ones(dilation_kernel, np.uint8)
+    img_dilation = cv2.dilate(my_image, kernel, iterations=1)
+    # cv2.imshow('dilated',img_dilation)
+    # cv2.waitKey(0)
+    
+    #find contours
+    ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    #sort contours
+    def func(ctr):
+        rect = cv2.boundingRect(ctr)
+        n = rect[1]
+        n = n + (5 - n % 10)
+        return n, rect[0]
+        
+    sorted_ctrs = sorted(ctrs, key=func)
+
+    contours_list = []
+    for i, ctr in enumerate(sorted_ctrs):
+        # Get bounding box
+        x, y, w, h = cv2.boundingRect(ctr)
+
+        # Getting ROI
+        # roi = my_image[y:y+h, x:x+w]
+
+        # show ROI
+        # cv2.imshow('segment no:'+str(i),roi)
+        # cv2.imwrite("segment_no_"+str(i)+".png",roi)
+            
+        contours_list.append(
+            [
+                x, y, w, h
+            ]
+        )
+    
+    if output_image is True:
+        return img_dilation
+    else:
+        return contours_list
+
+
+def detect_text(image, show_result=False):
+
+    st = time.time()
+
+    #grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('gray',gray)
+    # cv2.waitKey(0)
+
+    #binary
+    ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
+
+    edges = cv2.Canny(gray, 100, 200)
+    # cv2.imshow('edges', edges)
+    # cv2.waitKey(0)
+
+
+    img_dilation = do_something(edges, (3, 5), True)
+    contours_list = do_something(img_dilation, (1, 5), False)
+
+    # different form. not used.
+    # _contours_list = []
+    # for contour in contours_list:
+    #     x, y, w, h = contour
+    #     _contours_list.append([
+    #         x,
+    #         y,
+    #         x+w,
+    #         y+h,
+    #     ])
+
+
+    dt = time.time() - st
+    # print("[my_detect_text] time consuming:", dt)
+
+    if show_result:
+        for contour in contours_list:
+            x, y, w, h = contour
+            cv2.rectangle(image,(x,y),( x + w, y + h ),(90,0,255),2)
+
+        # cv2.imwrite('final_bounded_box_image.png',image)
+        cv2.imshow('marked areas',image)
+        cv2.waitKey(0)
+
+
+    cropped_images = []
+    for contour in contours_list:
+        x, y, w, h = contour
+        img = image[y:y+h, x:x+w]
+        # cv2.imshow('test', img)
+        # cv2.waitKey(0)
+        cropped_images.append(img)
+
+    return contours_list, cropped_images
+
+
+if __name__ == '__main__':
+    #import image
+    image = cv2.imread('in.png')
+    contours_list, cropped_images = detect_text(
+        image, 
+        show_result=True
+    )
+    print("len(contours_list):", len(contours_list))
+
---
+++ rapid_ocr_with_my_text_detector/task.py
@@ -0,0 +1,114 @@
+# !/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @File: task.py
+# @Time: 2021/03/07 20:29:32
+# @Author: SWHL
+# @Contact: liekkaskono@163.com
+import base64
+from functools import reduce
+import json
+import time
+
+import cv2
+import numpy as np
+
+from resources.rapidOCR import TextSystem, draw_text_det_res, check_and_read_gif
+
+det_model_path = 'resources/models/ch_PP-OCRv2_det_infer.onnx'
+cls_model_path = 'resources/models/ch_ppocr_mobile_v2.0_cls_infer.onnx'
+rec_model_path = 'resources/models/ch_ppocr_mobile_v2.0_rec_infer.onnx'
+
+text_sys = TextSystem(
+    det_model_path,
+    rec_model_path,
+    use_angle_cls=True,
+    cls_model_path=cls_model_path
+)
+
+
+def detect_recognize(image_path):
+    if isinstance(image_path, str):
+        image = cv2.imread(image_path)
+    elif isinstance(image_path, np.ndarray):
+        image = image_path
+    else:
+        raise TypeError(f'{image_path} is not str or ndarray.')
+
+    dt_boxes, rec_res, img, elapse_part = text_sys(image)
+
+    if dt_boxes is None or rec_res is None:
+        temp_rec_res = []
+        rec_res_data = temp_rec_res
+        elapse = 0
+        elapse_part = ''
+        image = cv2.imencode('.jpg', img)[1]
+        img = str(base64.b64encode(image))[2:-1]
+    else:
+        temp_rec_res = []
+        for i, value in enumerate(rec_res):
+            temp_rec_res.append([i, value[0], value[1]])
+        temp_rec_res = np.array(temp_rec_res)
+
+        rec_res_data = temp_rec_res.tolist()
+
+        # det_im = draw_text_det_res(dt_boxes, img)
+        # image = cv2.imencode('.jpg', det_im)[1]
+        # img = str(base64.b64encode(image))[2:-1]
+
+        # elapse = reduce(lambda x, y: float(x)+float(y), elapse_part)
+        # elapse_part = ','.join([str(x) for x in elapse_part])
+
+        elapse = sum(
+            [
+                *elapse_part.values()
+            ]
+        )
+
+        try:
+            dt_boxes = [i.tolist() for i in dt_boxes]
+        except AttributeError:
+            dt_boxes = dt_boxes
+
+    regions = []
+    for rect, res in zip(dt_boxes, rec_res_data):
+        regions.append(
+            {
+                "text": res[1],
+                "confidence": float(res[2]),
+                "rect": {
+                    "left": rect[0][0],
+                    "top": rect[0][1],
+                    "right": rect[1][0],
+                    "bottom": rect[1][1],
+                },
+            }
+        )
+        
+    lines = ""
+    _last_top = 0
+    for region in regions:
+        this_top = region["rect"]["top"]
+        if this_top - _last_top > 10:
+            lines += "\n"
+            lines += region["text"]
+        else:
+            lines += " "
+            lines += region["text"]
+
+        _last_top = this_top
+    
+    lines = lines.lstrip()
+    
+    return json.dumps({
+        # 'image': img,
+        'result': {
+            "lines": lines,
+            "regions": regions,
+        },
+        'info': {
+            'total_elapse': elapse,
+            'elapse_part': elapse_part,
+        },
+    })
+
+
